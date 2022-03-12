bd8d9893af49870491317c8566b156339ff32396
Yabebal Fantaye <yabebal@gmail.com>
2022-03-12 06:45:52 +0100

    sed opt bug fix

---
+++ compute/conda/install_challenge.sh
@@ -0,0 +1,30 @@
+home=${ADMIN_HOME:-$(bash ../get_home.sh)}
+
+#reload bashrc
+source $home/.bashrc
+
+#check conda is installed
+if ! command -v conda >/dev/null; then
+    echo "Install first conda"
+    exit 0
+fi
+
+#activate conda
+conda init bash
+source $home/.bashrc
+
+#create venv
+conda create -n algos -y
+conda activate algos
+
+#install common packages
+conda install \
+      -c conda-forge \
+      -y \
+      -q \
+      boto3 mysqlclient
+      
+conda install -c anaconda -y ipykernel
+pip install mlflow
+
+
