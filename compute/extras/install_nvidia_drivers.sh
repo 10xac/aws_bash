b4ade8306b7410efa8238945eff45b108ff5b854
Yabebal Fantaye <yabebal@gmail.com>
2022-06-02 13:52:58 +0200

    updated nvidea install

--- compute/extras/install_nvidia_drivers.sh
+++ compute/extras/install_nvidia_drivers.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+# Ref:
+# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/install-nvidia-driver.html
+
 scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cdir=$(dirname $scriptDir)
 home=${ADMIN_HOME:-$(bash $cdir/get_home.sh)}
@@ -10,13 +14,9 @@ source $home/.bashrc
 
 echo "installing nvidea driver .."
 if command -v apt-get >/dev/null; then
-    ubuntu-drivers autoinstall
-    apt install nvidia-driver-440
-    yum install -y gcc kernel-devel-$(uname -r)    
-elif command -v yum >/dev/null; then
-    yum install -y gcc kernel-devel-$(uname -r)
+    sudo apt-get install -y gcc make linux-headers-$(uname -r)
 
-cat << EOF | sudo tee --append /etc/modprobe.d/blacklist.conf
+    cat << EOF | sudo tee --append /etc/modprobe.d/blacklist.conf
 blacklist vga16fb
 blacklist nouveau
 blacklist rivafb
@@ -24,20 +24,33 @@ blacklist nvidiafb
 blacklist rivatv
 EOF
 
-sh -c "echo 'GRUB_CMDLINE_LINUX=\"rdblacklist=nouveau\"' >>  /etc/default/grub"
-
-grub2-mkconfig -o /boot/grub2/grub.cfg
-
-aws s3 cp --recursive s3://ec2-linux-nvidia-drivers/latest/ .
-
-chmod +x NVIDIA-Linux-x86_64*.run
-
-/bin/sh ./NVIDIA-Linux-x86_64*.run
+    # Edit the /etc/default/grub file and add the following line
+    sh -c "echo 'GRUB_CMDLINE_LINUX=\"rdblacklist=nouveau\"' >>  /etc/default/grub"    
+    
+elif command -v yum >/dev/null; then
+    yum install -y gcc kernel-devel-$(uname -r)
     
 else
     echo "unknown os system.."
     exit
 fi
+
+
+# Rebuild the Grub configuration.
+if command -v apt-get >/dev/null; then
+    sudo update-grub
+elif command -v yum >/dev/null; then    
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+fi
+
+# Download the GRID driver installation utility using the following command:
+aws s3 cp --recursive s3://ec2-linux-nvidia-drivers/latest/ .
+
+# Add permissions to run the driver installation utility using the following command.
+chmod +x NVIDIA-Linux-x86_64*.run
+
+# Run the self-install script as follows to install the GRID driver that you downloaded. 
+/bin/sh ./NVIDIA-Linux-x86_64*.run
 
 echo "nvidea drivers are successfully installed .. reboot to complete setup .."
 
