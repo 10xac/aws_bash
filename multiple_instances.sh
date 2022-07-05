1a45f2848448fbe783c08a65ce839d96ac6e14d2
Yabebal Fantaye <yabebal@gmail.com>
2022-07-05 06:13:47 +0200

    w9 machine create update

---
+++ multiple_instances.sh
@@ -0,0 +1,51 @@
+curdir=`pwd`
+
+
+#--------------------------------------------------------#
+###--------Define necessary environment variables-----##
+##------------------------------------------------------#
+if [ $# -lt 2 ]; then
+    echo "Usage: ecs_deploy <path to params file> <create/iplink> [[key=value]]"
+    exit 0
+fi
+
+paramsfile=$1; shift
+action=$1; shift
+echo "Using paramsfile=$paramsfile for action=$action .."
+
+#--- parse named arguments key=value
+for ARGUMENT in "$@"
+do
+   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+
+   KEY_LENGTH=${#KEY}
+   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+
+   export "$KEY"="$VALUE"
+done
+
+istart=${istart:-1}
+iend=${iend:-5}
+echo "number of instances to create: $istart to $iend"
+
+ec2script=${ec2script:-"ec2_compute.sh"}
+ipscript=${ipscript:-"postup/update_r53_record.sh"}
+
+exit
+
+#--------------------------------------------------------#
+###--------execute the scripts with input arguments------#
+##-------------------------------------------------------#
+for ((ig=$istart; ig<=$iend; i++)); do
+
+    if [ "$action" == "create" ]; then
+        echo "******creating instances for group=$ig********"
+        group=$ig bash $ec2script $paramsfile 
+    elif [ "$action" == "iplink" ]; then
+        echo "*********updating r53 record with IP for group=$ig******"        
+        group=$ig bash $ipscript $paramsfile
+    else
+        echo "*****unknown operation requested 1st argument=$1*****"
+    fi
+
+done
