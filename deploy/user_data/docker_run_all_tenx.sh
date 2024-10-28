7fb7da1fa0bf4931d6ca44010c4aa9fd697175bb
Yabebal Fantaye <yabebal@10academy.org>
2024-10-28 01:44:53 +0000

    adding working docker run all script

--- deploy/user_data/docker_run_all_tenx.sh
+++ deploy/user_data/docker_run_all_tenx.sh
@@ -8,27 +8,239 @@ fi
 cat <<EOF >>  $fout
 
 
-aws ecr get-login-password \
-    --region ${region} \
-| docker login \
-    --username AWS \
-    --password-stdin 070096167435.dkr.ecr.${region}.amazonaws.com
+#!/bin/bash
 
-image1="070096167435.dkr.ecr.us-east-1.amazonaws.com/prod-u2j-cms:latest"
-image2="070096167435.dkr.ecr.us-east-1.amazonaws.com/prod-u2j-cms:latest"
-docker pull $image1 
-docker pull $image2 
+# Ensure required environment variables are set
+if [ -z "$region" ] || [ -z "$emailsender" ] || [ -z "$appkey" ] || [ -z "$appkeysalt" ] || [ -z "$src_public_suffix_apply" ] || [ -z "$src_public_suffix" ]; then
+    echo "Please set all required environment variables before running the script."
+    exit 1
+fi
 
-dns1="u2jcms.10academy.org"
-dns2="u2jcms.10academy.org"
+# AWS ECR Login
+region="us-east-1"
+aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin 070096167435.dkr.ecr.${region}.amazonaws.com
+
+# Define Docker images
+apply_cms_image="070096167435.dkr.ecr.us-east-1.amazonaws.com/dev-apply-cms"
+apply_tenx_image="070096167435.dkr.ecr.us-east-1.amazonaws.com/dev-tenx-apply"
+cms_image="070096167435.dkr.ecr.us-east-1.amazonaws.com/prod-u2j-cms"
+tenx_image="070096167435.dkr.ecr.us-east-1.amazonaws.com/prod-u2j-tenx"
+
+# Pull Docker images
+docker pull $apply_cms_image
+docker pull $apply_tenx_image
+docker pull $cms_image
+docker pull $tenx_image
+
+# DNS and Database variables
+dns1="pai.applycms.10academy.org"
+dns2="pai.apply.10academy.org"
+dns3="pai.cms.10academy.org"
+dns4="pai.tenx.10academy.org"
+
+port1="1337"
+port2="5173"
+port3="5000"
+port4="3000"
+
+dbname_apply="paiapply"
+dbname_tenx="paiprod"
+
+# Set the required environment variables
+emailsender="u2j@10academy.org"
+appkey=$(echo $RANDOM$RANDOM$RANDOM$RANDOM | base64 | head -c 30; echo)
+appkeysalt=$(echo $RANDOM$RANDOM$RANDOM$RANDOM | base64 | head -c 30; echo)
+src_public_suffix_apply="prod-paiapply-cms"
+src_public_suffix="prod-pai-cms"
+s3bucket="s3://all-tenx-system"
 
 
-dockerenv1="-p 13q-env REDIRECT_URL=https://${dns_namespace} --env EMAIL_SENDER=${emailsender} --env DATABASE_NAME=${dbname} --env DATABASE_HOST=u2jdb.cluster-crlafpfc5g5y.us-east-1.rds.amazonaws.com -v /mnt/all-tenx-system/src-${src_public_suffix}:/opt/app/src -v /mnt/all-tenx-system/public-${src_public_suffix}:/opt/app/public --env appkey=$appkey --env appkeysalt=$appkeysalt --env APP_KEYS=${appkey},${appkeysalt} --env API_TOKEN_SALT=$appkeysalt --env ADMIN_JWT_SECRET=$appkey"
+# Docker environment configurations
+dockerenv1="-p $port1:$port1 --env REDIRECT_URL=https://${dns1} --env EMAIL_SENDER=${emailsender} --env DATABASE_NAME=${dbname_apply} --env DATABASE_HOST=u2jdb.cluster-crlafpfc5g5y.us-east-1.rds.amazonaws.com -v /mnt/all-tenx-system/src-${src_public_suffix_apply}:/opt/app/src -v /mnt/all-tenx-system/public-${src_public_suffix_apply}:/opt/app/public --env appkey=$appkey --env appkeysalt=$appkeysalt --env APP_KEYS=${appkey},${appkeysalt} --env API_TOKEN_SALT=$appkeysalt --env ADMIN_JWT_SECRET=$appkey"
 
-dockerenv2="-p 1337:1337 --env REDIRECT_URL=https://${dns_namespace} --env EMAIL_SENDER=${emailsender} --env DATABASE_NAME=${dbname} --env DATABASE_HOST=u2jdb.cluster-crlafpfc5g5y.us-east-1.rds.amazonaws.com -v /mnt/all-tenx-system/src-${src_public_suffix}:/opt/app/src -v /mnt/all-tenx-system/public-${src_public_suffix}:/opt/app/public --env appkey=$appkey --env appkeysalt=$appkeysalt --env APP_KEYS=${appkey},${appkeysalt} --env API_TOKEN_SALT=$appkeysalt --env ADMIN_JWT_SECRET=$appkey"
+dockerenv2="-p $port2:$port2 --env REDIRECT_URL=https://${dns2} --env EMAIL_SENDER=${emailsender} --env DATABASE_NAME=${dbname_apply} --env DATABASE_HOST=u2jdb.cluster-crlafpfc5g5y.us-east-1.rds.amazonaws.com -v /mnt/all-tenx-system/src-${src_public_suffix_apply}:/opt/app/src -v /mnt/all-tenx-system/public-${src_public_suffix_apply}:/opt/app/public --env appkey=$appkey --env appkeysalt=$appkeysalt --env APP_KEYS=${appkey},${appkeysalt} --env API_TOKEN_SALT=$appkeysalt --env ADMIN_JWT_SECRET=$appkey"
+
+dockerenv3="-p $port3:$port3 --env REDIRECT_URL=https://${dns3} --env EMAIL_SENDER=${emailsender} --env DATABASE_NAME=${dbname_tenx} --env DATABASE_HOST=u2jdb.cluster-crlafpfc5g5y.us-east-1.rds.amazonaws.com -v /mnt/all-tenx-system/src-${src_public_suffix}:/opt/app/src -v /mnt/all-tenx-system/public-${src_public_suffix}:/opt/app/public --env appkey=$appkey --env appkeysalt=$appkeysalt --env APP_KEYS=${appkey},${appkeysalt} --env API_TOKEN_SALT=$appkeysalt --env ADMIN_JWT_SECRET=$appkey"
+
+dockerenv4="-p $port4:$port4 --env REDIRECT_URL=https://${dns4} --env EMAIL_SENDER=${emailsender} --env DATABASE_NAME=${dbname_tenx} --env DATABASE_HOST=u2jdb.cluster-crlafpfc5g5y.us-east-1.rds.amazonaws.com -v /mnt/all-tenx-system/src-${src_public_suffix}:/opt/app/src -v /mnt/all-tenx-system/public-${src_public_suffix}:/opt/app/public --env appkey=$appkey --env appkeysalt=$appkeysalt --env APP_KEYS=${appkey},${appkeysalt} --env API_TOKEN_SALT=$appkeysalt --env ADMIN_JWT_SECRET=$appkey"
+
+# Run Docker containers
+docker run $dockerenv1 -d $apply_cms_image
+docker run $dockerenv2 -d $apply_tenx_image
+docker run $dockerenv3 -d $cms_image
+docker run $dockerenv4 -d $tenx_image
+
+EOF
+
+cat <<'EOF' >>  $fout 
 
 
-docker run $dockerenv1 -d $image1
-docker run $dockerenv2 -d $image2
+cat <<'EOF2' > /etc/nginx/conf.d/apply_cms.conf
+server {
+       listen 80;
+       server_name apply.10academy.org;
+       server_tokens off;
+
+
+       location / {
+        	 return 301 https://$host$request_uri;
+    		 }
+}
+
+
+server {
+    listen 443 ssl;
+    server_name apply.10academy.org;
+    server_tokens off;
+
+    ssl_certificate /etc/ssl/ssl-bundle.crt;
+    ssl_certificate_key /etc/ssl/my-aws-private.key;
+
+    # Redirect non-https traffic to https
+    if ($scheme != "https") {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+    
+    location / {
+        proxy_pass http://0.0.0.0:5173;
+        #proxy_set_header Host $host;
+        proxy_set_header    Host                $http_host;
+        proxy_set_header    X-Real-IP           $remote_addr;
+        proxy_set_header    X-Forwarded-For     $proxy_add_x_forwarded_for;
+    }
+    
+}
+EOF2
+
+
+cat <<'EOF2' > /etc/nginx/conf.d/apply.conf
+server {
+       listen 80;
+       server_name apply.10academy.org;
+       server_tokens off;
+
+
+       location / {
+        	 return 301 https://$host$request_uri;
+    		 }
+}
+
+
+server {
+    listen 443 ssl;
+    server_name apply.10academy.org;
+    server_tokens off;
+
+    ssl_certificate /etc/ssl/ssl-bundle.crt;
+    ssl_certificate_key /etc/ssl/my-aws-private.key;
+
+    # Redirect non-https traffic to https
+    if ($scheme != "https") {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+    
+    location / {
+        proxy_pass http://0.0.0.0:5173;
+        #proxy_set_header Host $host;
+        proxy_set_header    Host                $http_host;
+        proxy_set_header    X-Real-IP           $remote_addr;
+        proxy_set_header    X-Forwarded-For     $proxy_add_x_forwarded_for;
+    }
+    
+}
+EOF2
+
+
+cat <<'EOF2' > /etc/nginx/conf.d/tenx_cms.conf
+server {
+       listen 80;
+       server_name apply.10academy.org;
+       server_tokens off;
+
+
+       location / {
+        	 return 301 https://$host$request_uri;
+    		 }
+}
+
+
+server {
+    listen 443 ssl;
+    server_name apply.10academy.org;
+    server_tokens off;
+
+    ssl_certificate /etc/ssl/ssl-bundle.crt;
+    ssl_certificate_key /etc/ssl/my-aws-private.key;
+
+    # Redirect non-https traffic to https
+    if ($scheme != "https") {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+    
+    location / {
+        proxy_pass http://0.0.0.0:5173;
+        #proxy_set_header Host $host;
+        proxy_set_header    Host                $http_host;
+        proxy_set_header    X-Real-IP           $remote_addr;
+        proxy_set_header    X-Forwarded-For     $proxy_add_x_forwarded_for;
+    }
+    
+}
+EOF2
+
+
+cat <<'EOF2' > /etc/nginx/conf.d/tenx.conf
+server {
+       listen 80;
+       server_name apply.10academy.org;
+       server_tokens off;
+
+
+       location / {
+        	 return 301 https://$host$request_uri;
+    		 }
+}
+
+
+server {
+    listen 443 ssl;
+    server_name apply.10academy.org;
+    server_tokens off;
+
+    ssl_certificate /etc/ssl/ssl-bundle.crt;
+    ssl_certificate_key /etc/ssl/my-aws-private.key;
+
+    # Redirect non-https traffic to https
+    if ($scheme != "https") {
+        return 301 https://$host$request_uri;
+    } # managed by Certbot
+    
+    location / {
+        proxy_pass http://0.0.0.0:5173;
+        #proxy_set_header Host $host;
+        proxy_set_header    Host                $http_host;
+        proxy_set_header    X-Real-IP           $remote_addr;
+        proxy_set_header    X-Forwarded-For     $proxy_add_x_forwarded_for;
+    }
+    
+}
+EOF2
+
+
+# sed replace server name with the correct server name
+
+sed -i "s/apply.10academy.org/${dns1}/g" /etc/nginx/conf.d/apply_cms.conf
+sed -i "s/apply.10academy.org/${dns2}/g" /etc/nginx/conf.d/apply.conf
+sed -i "s/apply.10academy.org/${dns3}/g" /etc/nginx/conf.d/tenx_cms.conf
+sed -i "s/apply.10academy.org/${dns4}/g" /etc/nginx/conf.d/tenx.conf
+
+sed -i "s/5173/${port1}/g" /etc/nginx/conf.d/apply_cms.conf
+sed -i "s/5173/${port2}/g" /etc/nginx/conf.d/apply.conf
+sed -i "s/5173/${port3}/g" /etc/nginx/conf.d/tenx_cms.conf
+sed -i "s/5173/${port4}/g" /etc/nginx/conf.d/tenx.conf
+
+# Restart Nginx
+systemctl daemon-reload
+systemctl restart nginx
 
 EOF
--- deploy/user_data/install_mongodb_ubuntu.sh
+++ deploy/user_data/install_mongodb_ubuntu.sh
@@ -7,7 +7,7 @@
 	# System recommendation : Ubuntu 16.04	
 	# Download the script
 	# Open in bash and make it executable with command: chmod +x mongodb_install.sh
-	# Run with command: sudo ./mongodb_install.sh
+	# Run with command: ./mongodb_install.sh
 
 # OTHER USEFUL SCRIPTS:
 	# For making a MongoDB replica set with three nodes: https://gist.github.com/Maria-UET/af4332f6dd9e57f2d0f6141dbb8dd447
@@ -23,90 +23,148 @@ fi
 
 cat <<'EOF' >>  $fout
 
+
 # Add an appropriate username for your MongoDB user
+# Add an appropriate password for your MongoDB user. 
+# Password should be ideally read from a config.ini file, 
+# keeping passwords in bash scripts is not secure.
 USR="admin"
-# Add an appropriate password for your MongoDB user. Password should be ideally read from a config.ini file, keeping passwords in bash scripts is not secure.
 PASS="test@admin"
-DB="AIQEMAd"
+DB="AuthDB"
+DBDATA="AiQEMAd"
 ROLE="root"
 BIND_IP=0.0.0.0
+BUCKET="s3://tgad-mongodb-backup"
 
-sudo apt update
+function restore_latest_backup {
+        KEY=$(aws s3 ls $BUCKET/tgad_mongodb_backup/ --recursive  | sort | tail -n 1 | awk '{print $4}')
+        backup_filename=$(basename $KEY)
+        echo "=============STARTING RESTORE=================="
+        echo "Getting latest backup from s3 $BUCKET/$KEY .."
+        aws s3 cp $BUCKET/$KEY ./ --recursive
+
+        echo "Restoring $DB "
+        mongorestore --gzip --archive=$backup_filename --nsInclude="$DBDATA.*" --drop
+        echo "Done Restoring!"
+		echo "=============RESTORE COMPLETED=================="
+}
+
+
+## Write a bash script to schedule a cron job to 
+## backup the mongodb database and store it in the s3 bucket
+cat <<EOFF > $HOME/mongodb_backup.sh
+#!/bin/bash
+
+# Get the current date and time
+datetime=$(date +"%Y%m%d_%H%M%S")
+
+echo "$(date +"%Y-%m-%d_%H-%M-%S"): Backing up in $latest_backup" >> log.txt
+
+latest_backup="aiqemad_mongodb_backup_latest.zip"
+mongodump --db $DB  --gzip --archive > $latest_backup
+
+aws s3 cp $latest_backup $BUCKET/tgad_mongodb_backup/mongodb_backup_$datetime.zip
+
+echo "$(date +"%Y-%m-%d_%H-%M-%S"): Backed up!" >> log.txt
+EOFF
+
+echo ""
+echo "Setting up the cron job to backup the mongodb database"
+chmod +x $HOME/mongodb_backup.sh
+(crontab -l 2>/dev/null; echo "0 0/6 * * * $HOME/mongodb_backup.sh") | crontab -
+
+
 
 echo ""
 echo "installing mongodb dependencies"
-sudo apt install gnupg wget apt-transport-https ca-certificates software-properties-common
+apt-get -y install gnupg wget apt-transport-https ca-certificates software-properties-common
 
 echo ""
 echo "Adding the MongoDB GPG key to your system"
-wget -qO- https://pgp.mongodb.com/server-7.0.asc | gpg --dearmor | sudo tee /usr/share/keyrings/mongodb-server-7.0.gpg >/dev/null
+wget -qO- https://www.mongodb.org/static/pgp/server-8.0.asc | gpg --dearmor | tee /usr/share/keyrings/mongodb-server-8.0.gpg >/dev/null
+
 
 echo ""
 echo "Creating a deb list file for MongoDB"
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/7.0 multiverse" | sudo tee -a /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu $(lsb_release -cs)/mongodb-org/8.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+
 
 echo ""
 echo "Installing MongoDB"
-sudo apt update
-sudo apt install mongodb-org
+apt-get -y update 
+apt-get -y --force-yes install mongodb-org mongodb-org-database \
+				  mongodb-org-server mongodb-mongosh \
+				  mongodb-org-mongos mongodb-org-tools
+
+echo ""
+echo "Set the right permission settings"
+chown -R mongodb:mongodb /var/lib/mongodb
+service mongod restart
 
 echo ""
 echo "Starting MongoDB"
-sudo systemctl enable mongod.service
-sudo systemctl status mongod.service
+systemctl enable mongod.service
+systemctl status mongod.service
+
+export LC_ALL=C
 
 echo ""
 echo " ############################## Mongodb has been installed  ###############################"
-echo " ##      Check status of mongod server by running this command: sudo netstat -plntu      ##"
+echo " ##      Check status of mongod server by running this command: netstat -plntu      ##"
 echo " ##########################################################################################"
 mongosh --eval 'db.runCommand({ connectionStatus: 1 })'
 
 
-export LC_ALL=C
-
 
 echo ""
-echo "Creating mongodb user"
+echo "Restore from latest backup"
+restore_latest_backup
+
+echo ""
+echo "Creating a user with name=$USR and role=$ROLE:"
 mongo admin --eval "db.createUser({'user':'$USR', 'pwd':'$PASS', 'roles':[{'role':'$ROLE', 'db':'$DB'}]})"
 
 
-sudo systemctl stop mongod
+systemctl stop mongod
 
 
 echo ""
 echo "Configuring the mongod.conf file to update bindip and enable authentication"
-sudo sed -i[bindIp] "s/bindIp: /bindIp: $BIND_IP #/g" /etc/mongod.conf 
+sed -i[bindIp] "s/bindIp: /bindIp: $BIND_IP #/g" /etc/mongod.conf 
 # Do not enable ip_bind_all without enabling authorization. otherwise, the db will be exposed.
-sudo echo  "#authorization config
+echo  "#authorization config
 security:
-   authorization: enabled" >> /etc/mongod.conf
-
+   authorization: enabled" >>  /etc/mongod.conf
 
 echo ""
 echo "Appending --auth to mongod.service to enable authentication"
-sudo sed -i '/ExecStart/ s/$/ --auth/' /lib/systemd/system/mongod.service
+sed -i '/ExecStart/ s/$/ --auth/' /lib/systemd/system/mongod.service
 
-sudo systemctl enable mongod.service
+systemctl enable mongod.service
 
 
 echo ""
 echo "Initiating daemon-reload"
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 
-sudo service mongod stop
+service mongod stop
 
 
 echo ""
 echo "Starting the mongod server with the following parameters:"
-echo "sudo mongod --bind_ip_all --fork --logpath /var/log/mongodb.log"
-sudo mongod --bind_ip $BIND_IP --fork --logpath /var/log/mongodb.log
+echo "mongod --bind_ip_all --fork --logpath /var/log/mongodb.log"
+mongod --bind_ip $BIND_IP --fork --logpath /var/log/mongodb.log
 
 
 echo "Starting the mongo shell with following parameters:"
 echo "mongo -u $USR -p $PASS"
 mongo -u $USR -p $PASS 
 
+chown -R mongodb:mongodb /var/lib/mongodb
+chown mongodb:mongodb /tmp/mongodb-27017.sock
+service mongod restart
 
 
 echo "You can connect to this db from remote client using: mongo --username $USR --password $PASS ipaddress:27017/db_name --authenticationDatabase $DB"
